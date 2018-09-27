78e8d03c7d6c46dfeb15b621a6106e1cf280d524
Bruno Romero <brunoromero172@hotmail.com>
2018-09-27 16:56:15 -0300

    added some helper functions and tests

--- src/lox/scanner/helpers.clj
+++ src/lox/scanner/helpers.clj
@@ -5,3 +5,18 @@
     (:source)
     (nth (:current scanner))
     (str)))
+
+(defn advance [scanner]
+  (update-in scanner [:current] inc))
+
+(defn peek
+  ([scanner] (peek scanner 1))
+  ([scanner amount]
+   (loop [scanner scanner
+          amount amount]
+     (if (>= 0 amount)
+       (current-char scanner)
+       (recur (advance scanner) (dec amount))))))
+
+(defn end? [scanner]
+  (>= (:current scanner) (count (:source scanner))))
--- test/lox/scanner/helpers_test.clj
+++ test/lox/scanner/helpers_test.clj
@@ -7,3 +7,27 @@
   (testing "current-char should return the current char of a scanner if found"
     (let [scanner (s/create-scanner "var a = 10;")]
       (is "v" (h/current-char scanner)))))
+
+(deftest advance
+  (testing "advance should move the pointer to the next char"
+    (let [scanner (h/advance (s/create-scanner "var a = 10;"))]
+      (is "a" (h/current-char scanner)))))
+
+(deftest peek
+  (testing "peek returns the char after the current one if not amount is passed"
+    (let [scanner (s/create-scanner "var a = 10;")]
+      (is "a" (h/peek scanner))))
+
+  (testing "peek returns the amount of chars after the current one if amount is passed"
+    (let [scanner (s/create-scanner "var a = 10;")]
+      (is "=" (h/peek scanner 6)))))
+
+(deftest end?
+  (testing "end? returns true if scanner is at/after the last position"
+    (let [source "var a = 10;"
+          scanner (assoc-in (s/create-scanner source) [:current] (count source))]
+      (is (true? (h/end? scanner)))))
+
+  (testing "end? returns false if scanner is before the last position"
+    (let [scanner (s/create-scanner "var a = 10;")]
+      (is (false? (h/end? scanner))))))
